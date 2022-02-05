f389d168469cbe1c609fd7291512ba3f219783af
eugenelgm <eugenelgm@gmail.com>
2022-02-05 20:57:00 +0900

    Add files via upload
    first init

---
+++ src/test/java/TestSample.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class TestSample {
+
+    @Test
+    public void test1() {
+        System.out.println("Test!");
+    }
+
+    @Test
+    public void testUrlConnection() {
+        URL url = null;
+        try {
+            url = new URL("http://www.android.com/");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            StringBuffer buffer = readStream(urlConnection);
+            System.out.println(buffer.toString());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+
+        }
+    }
+
+    private StringBuffer readStream(HttpURLConnection con) throws IOException {
+        Charset charset = Charset.forName("UTF-8");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(),charset));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response;
+    }
+}
